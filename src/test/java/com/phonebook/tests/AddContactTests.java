763e77122bad92752a7e5872b825e3482bf7408e
aliyasagut <aliya.sagut@gmail.com>
2024-03-05 23:17:12 +0100

    add lesson8

--- src/test/java/com/phonebook/tests/AddContactTests.java
+++ src/test/java/com/phonebook/tests/AddContactTests.java
@@ -5,7 +5,13 @@ import com.phonebook.models.User;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class AddContactTests extends TestBase{
 
@@ -39,5 +45,57 @@ public class AddContactTests extends TestBase{
     @AfterMethod
     public void postCondition() {
         app.getContact().removeContact();
+    }
+
+    @DataProvider
+    public Iterator<Object[]> addContact() {
+        List<Object[]> list = new ArrayList<>();
+        list.add(new Object[]{"Oliver", "Kan", "1234567890", "kan@kan.kan", "Berlin", "test description"});
+        list.add(new Object[]{"Oliver1", "Kan1", "12234567890", "kan1@kan.kan", "Berlin1", "test description1"});
+        list.add(new Object[]{"Oliver2", "Kan2", "111234567890", "kan2@kan.kan", "Berlin2", "test description2"});
+
+        return list.iterator();
+    }
+
+    @Test(dataProvider = "addContact")
+    public void addContactPositiveTestFromDataProvider(String name, String lastname, String phone, String email, String address, String description) {
+        Contact adam = new Contact()
+                .setName(name)
+                .setLastname(lastname)
+                .setPhone(phone)
+                .setEmail(email)
+                .setAddress(address)
+                .setDescription(description);
+        app.getContact().clickOnAddLink();
+        app.getContact().fillContactForm(adam);
+        app.getContact().clickOnSaveButton();
+        Assert.assertTrue(app.getContact().isContactCreatedByText(name));
+    }
+
+    @DataProvider
+    public Iterator<Object[]> addContactFromCsv() throws IOException {
+        List<Object[]> list = new ArrayList<>();
+
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contact.csv")));
+
+        String line = reader.readLine();
+
+        while (line != null) {
+
+            String[] split = line.split(",");
+            list.add(new Object[]{new Contact().setName(split[0]).setLastname(split[1]).setPhone(split[2])
+                    .setEmail(split[3]).setAddress(split[4]).setDescription(split[5])});
+            line = reader.readLine();
+        }
+
+        return list.iterator();
+    }
+
+    @Test(dataProvider = "addContactFromCsv")
+    public void addContactPositiveTestFromDataProviderWithFile(Contact contact) {
+        app.getContact().clickOnAddLink();
+        app.getContact().fillContactForm(contact);
+        app.getContact().clickOnSaveButton();
+        Assert.assertTrue(app.getContact().isContactCreatedByText(contact.getName()));
     }
 }
